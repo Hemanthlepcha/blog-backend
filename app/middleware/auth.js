46f4494bfe4475c85cb340d38dc08e277c287100
Hemanthlepcha <hemanthlepcha0@gmail.com>
2024-09-27 21:14:49 +0600

    right namings

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,26 +2,20 @@ import { tokenDecoder, extractToken } from "../helper/jwt.js";
 
 export const auth = (req, res, next) => {
   try {
-    // Extract token from the request
     const tokenFromReq = extractToken(req);
 
-    // Check if token is present
     if (!tokenFromReq) {
       return res
         .status(401)
         .json({ message: "Access denied. No token provided." });
     }
 
-    // Decode the token
-    const decoded = tokenDecoder(tokenFromReq);
+    const decodedToken = tokenDecoder(tokenFromReq);
 
-    // Attach decoded token data (usually user info) to the request object
-    req.user = decoded;
+    req.user = decodedToken;
 
-    // Proceed to the next middleware/route handler
     next();
   } catch (error) {
-    // Handle token errors (e.g., invalid or expired token)
     return res
       .status(403)
       .json({ message: "Invalid token. Access denied.", error: error.message });
